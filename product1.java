a317bb8d081e36d8a6469334ce111cbf65fb980a
github_SaiTarunTeja <github_saitarunteja1729@gmail.com>
2023-08-18 15:07:00 +0530

    first commit

---
+++ product1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class product1
+{
+public static void main(String args[])
+   {
+     Scanner s = new Scanner(System.in);
+     System.out.println("Shop Any Thing");
+     String a = s.next();
+     String b = s.next();
+     String c = s.next();
+     String d = s.next();
+
+   
+     System.out.println(a +" =50");
+     System.out.println(b +" =50");
+     System.out.println(c +" =50");
+     System.out.println(d +" =50");
+     System.out.println("total = 200");
+     System.out.println("discount = 40");
+     System.out.println("your total bill = 160");
+   }
+}
